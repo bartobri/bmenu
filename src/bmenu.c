c415fbc06b6d93ce6783d8da101dceaca0b6c427
Brian Barto <bartobrian@gmail.com>
2016-03-22 17:43:25 -0400

    Some big code changes. Moved some things around. Improved how the menu is drawn. Much cleaner and simpler this way. Still need some add a menu border and inner header.
    	modified:   src/bmenu.c

--- src/bmenu.c
+++ src/bmenu.c
@@ -33,35 +33,20 @@ char menu[MAX_MENU_OPTIONS][MAX_MENU_CHAR];
 char command[MAX_MENU_OPTIONS][MAX_COMMAND_CHAR];
 int menuRows = 0, menuCols = 0;
 
-/*
+// Making window dimentions global
+int windowRows, windowCols;
+
+
+/***************************
  * Main function
- */
+ ***************************/
 int main (void) {
 	int loadMenuConfig(void);
-
-	// Get terminal size
-	int wRows, wCols;
-	struct winsize w;                                                                                         
-	ioctl(0, TIOCGWINSZ, &w);
-	wRows =  w.ws_row;
-	wCols = w.ws_col;
-
-	// initializing window with all spaces
-	char window[wRows][wCols];
-
-	for (int c1 = 0; c1 < wRows; ++c1) {
-		int c2;
-		for (c2 = 0; c2 < wCols - 1; ++c2)
-			window[c1][c2] = SPACE;
-		window[c1][c2] = NEWLINE;
-	}
-
-	// Printing (clearing) window
-	for (int c1 = 0; c1 < wRows; ++c1)
-		for (int c2 = 0; c2 < wCols; ++c2)
-			printf("%c", window[c1][c2]);
+	void printOuterHeader(void);
+	void printMenu(int);
 
 	// Getting menu config
+	// TODO - see if I can remove the use of globals in loadMenuConfig()
 	int result = loadMenuConfig();
 	if (result == 1) {
 		fprintf(stderr, "Please set HOME environment variable.\n");
@@ -71,84 +56,25 @@ int main (void) {
 		return result;
 	}
 
-	// Determining starting row and column
-	// TODO - consider longest line length may be longer
-	//        than window width resulting in a negative
-	//        starting point.
-	int startRow, startCol;
-	startCol = (wCols / 2) - (menuCols / 2);
-	startRow = (wRows / 2) - (menuRows / 2);
+	// Get terminal window size
+	struct winsize w;                                                                                         
+	ioctl(0, TIOCGWINSZ, &w);
+	windowRows =  w.ws_row;
+	windowCols = w.ws_col;
 
-	// Inserting menu in to window
-	for (int line = 0; line < menuRows; ++line)
-		for (int charCount = 0; menu[line][charCount] != '\0'; ++charCount)
-			window[line + startRow][(charCount) + startCol] = menu[line][charCount];
-
-	// Menu Outer Head
-	window[1][2] = 'B';
-	window[1][3] = '-';
-	window[1][4] = 'M';
-	window[1][5] = 'E';
-	window[1][6] = 'N';
-	window[1][7] = 'U';
-	for (int col = 0; window[2][col] != '\n'; ++col)
-		window[2][col] = '=';
-
-	// Menu Inner Head and Border
-	int menuColPad = 4;
-	int menuRowPad = 2;
-	if (startRow - menuRowPad > 1 && startCol - menuColPad > 1) {
-		int borderStartRow = startRow - menuRowPad;
-		int borderStartCol = startCol - menuColPad;
-		int borderEndRow = startRow + menuRows + (menuRowPad - 1);
-		int borderEndCol = startCol + menuCols + (menuColPad - 1);
-		window[borderStartRow - 1][borderStartCol] = 'C';
-		window[borderStartRow - 1][borderStartCol + 1] = 'h';
-		window[borderStartRow - 1][borderStartCol + 2] = 'o';
-		window[borderStartRow - 1][borderStartCol + 3] = 'o';
-		window[borderStartRow - 1][borderStartCol + 4] = 's';
-		window[borderStartRow - 1][borderStartCol + 5] = 'e';
-		window[borderStartRow - 1][borderStartCol + 7] = 'O';
-		window[borderStartRow - 1][borderStartCol + 8] = 'p';
-		window[borderStartRow - 1][borderStartCol + 9] = 't';
-		window[borderStartRow - 1][borderStartCol + 10] = 'i';
-		window[borderStartRow - 1][borderStartCol + 11] = 'o';
-		window[borderStartRow - 1][borderStartCol + 12] = 'n';
-		for (int i1 = borderStartRow; i1 <= borderEndRow; ++i1)
-			for (int i2 = borderStartCol; i2 <= borderEndCol; ++i2)
-
-				// Upper left corner
-				if (i1 == borderStartRow && i2 == borderStartCol)
-					window[i1][i2] = '=';
-
-				// Upper right corner
-				else if (i1 == borderStartRow && i2 == borderEndCol)
-					window[i1][i2] = '=';
-
-				// Lower left corner
-				else if (i1 == borderEndRow && i2 == borderStartCol)
-					window[i1][i2] = '=';
-
-				// Lower right corner
-				else if (i1 == borderEndRow && i2 == borderEndCol)
-					window[i1][i2] = '=';
-
-				// Top row
-				else if (i1 == borderStartRow)
-					window[i1][i2] = '=';
-
-				// Bottom row
-				else if (i1 == borderEndRow)
-					window[i1][i2] = '=';
-
-				// Left edge
-				else if (i2 == borderStartCol)
-					window[i1][i2] = '|';
-
-				// Right edge
-				else if (i2 == borderEndCol)
-					window[i1][i2] = '|';
+	// initializing terminal window with all spaces
+	for (int c1 = 0; c1 < windowRows; ++c1) {
+		int c2;
+		for (c2 = 0; c2 < windowCols - 1; ++c2) {
+			printf("\033[%i;%iH", c1, c2);
+			printf("%c", SPACE);
+		}
+		printf("\033[%i;%iH", c1, c2);
+		printf("%c", NEWLINE);
 	}
+
+	// Outer header
+	printOuterHeader();
 
 	// Setting terminal input mode to turn off echo and buffering
 	static struct termios oldt, newt;
@@ -171,22 +97,11 @@ int main (void) {
 					++menuCurOption;
 		}
 
-		// printing window
-		for (int c1 = 0; c1 < wRows; ++c1)
-			for (int c2 = 0; c2 < wCols; ++c2) {
+		// Print menu with the current selection highlighted
+		printMenu(menuCurOption);
 
-				// Set cursor position
-				printf("\033[%i;%iH", c1, c2);
-
-				if (c1 == startRow + (menuCurOption - 1) && c2 > startCol - 1 && c2 < startCol + menuCols) {
-					printf(KMAG "%c", window[c1][c2]);
-					if (c2 == startCol) {
-						printf("\033[%i;%iH", c1, c2 - 2);
-						printf(KMAG "*");
-					}
-				} else
-					printf(KNRM "%c", window[c1][c2]);
-			}
+		// Position cursor at the bottom of the terminal window
+		printf("\033[%i;%iH", windowRows, 1);
 
 	} while ((input = getchar()) != ENTER);
 
@@ -200,10 +115,10 @@ int main (void) {
 	return 0;
 }
 
-/*
+/*************************************************
  * loadMenuConfig() - Loading the menu config file.
  * Return a non-zero result if anything goes wrong.
- */
+ *************************************************/
 int loadMenuConfig(void) {
 	char *homeDir = getenv("HOME");;
 
@@ -277,4 +192,33 @@ int loadMenuConfig(void) {
 	fclose(menuConfig);
 
 	return 0;
+}
+
+void printMenu(int o) {
+	int startRow = 1, startCol = 1;
+
+	// Determining starting row and column for menu
+	startCol = ((windowCols / 2) - (menuCols / 2)) > 0 ? ((windowCols / 2) - (menuCols / 2)) : 0;
+	startRow = ((windowRows / 2) - (menuRows / 2)) > 0 ? ((windowRows / 2) - (menuRows / 2)) : 0;
+
+	// Inserting menu in to terminal window
+	for (int row = 0; row < menuRows; ++row) {
+		if (row == o - 1)
+			printf(KMAG);
+		else
+			printf(KNRM);
+		for (int col = 0; menu[row][col] != '\0'; ++col)
+			printf("\033[%i;%iH%c", row + startRow, col + startCol, menu[row][col]);
+	}
+}
+
+void printOuterHeader(void) {
+	int textRow = 1;
+	int barRow = 2;
+	int barChar = '=';
+
+	printf("\033[%i;%iH%s", textRow, 2, "B-MENU");
+
+	for (int col = 0; col < windowCols; ++col)
+		printf("\033[%i;%iH%c", barRow, col, barChar);
 }
