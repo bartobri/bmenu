451466d4af0b684c4359c79b07f500d46e33c8ad
Brian Barto <bartobrian@gmail.com>
2016-03-22 17:53:05 -0400

    Needed alignment Prettiness
    	modified:   src/bmenu.c

--- src/bmenu.c
+++ src/bmenu.c
@@ -4,29 +4,29 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>     // getenv()
-#include <sys/ioctl.h>  // Support for terminal dimentions
-#include <termios.h>    // Support for character input
-#include <unistd.h>     // Support for character input
+#include <stdlib.h>          // getenv()
+#include <sys/ioctl.h>       // Support for terminal dimentions
+#include <termios.h>         // Support for character input
+#include <unistd.h>          // Support for character input
 
-#define MENU_CONFIG		".bmenu"
+#define MENU_CONFIG	         ".bmenu"
 
-#define MAX_MENU_OPTIONS	10
-#define MAX_MENU_CHAR		50
-#define MAX_COMMAND_CHAR	100
+#define MAX_MENU_OPTIONS     10
+#define MAX_MENU_CHAR        50
+#define MAX_COMMAND_CHAR     100
 
-#define SPACE	040
-#define NEWLINE 012
-#define ENTER	012
+#define SPACE                040
+#define NEWLINE              012
+#define ENTER                012
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define KNRM                 "\x1B[0m"
+#define KRED                 "\x1B[31m"
+#define KGRN                 "\x1B[32m"
+#define KYEL                 "\x1B[33m"
+#define KBLU                 "\x1B[34m"
+#define KMAG                 "\x1B[35m"
+#define KCYN                 "\x1B[36m"
+#define KWHT                 "\x1B[37m"
 
 // Making menu vars global
 char menu[MAX_MENU_OPTIONS][MAX_MENU_CHAR];
